0331b5ca2a5482b941e7a764b9fb4d234325452d
nkrishnakishor <30660903+nkrishnakishor@users.noreply.github.com>
2018-08-06 11:38:09 +0530

    Add files via upload

---
+++ Using_filer.ps1
@@ -0,0 +1,3 @@
+﻿foreach ( $user in Get-Content C:\hosts.txt) {
+    Get-ADUser -Filter { employeenumber -like $user } -properties * | select employeenumber,samaccountname | Export-Csv -Append -Force C:\users_details.csv
+    }
